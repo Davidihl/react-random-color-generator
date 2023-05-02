a106d1c70ab7338be4a560b1c2a1d758fc50fb44
David Ihl <daih1985@gmail.com>
2023-05-02 16:34:01 +0200

    Initialize with random color

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
+import randomColor from 'randomcolor';
+import { useState } from 'react';
+
 export default function App() {
+  const [color, setColor] = useState(randomColor());
+  console.log(randomColor());
+
   return (
     <div
       key="outerDiv"
@@ -7,15 +13,25 @@ export default function App() {
       <div className="flex flex-col gap-4 w-80">
         <div
           key="colorDiv"
-          className="w-80 h-40 bg-blue-300 flex justify-center items-center rounded"
+          className="max-w-80 h-40 flex justify-center items-center rounded"
+          style={{ backgroundColor: `${color}` }}
         >
-          <span className="bg-white p-4 rounded text-sm font-bold">
-            #123456
+          <span
+            className="bg-white p-4 rounded text-sm font-bold"
+            style={{ color: `${color}` }}
+          >
+            {color}
           </span>
         </div>
         <form className="flex gap-2 bg-gray-300 p-2 rounded drop-shadow">
-          <input className="border border-gray-600 p-2 rounded w-1/3" />
-          <input className="border border-gray-600 p-2 rounded w-1/3" />
+          <input
+            key="hue"
+            className="border border-gray-600 p-2 rounded w-1/3"
+          />
+          <input
+            key="luminosity"
+            className="border border-gray-600 p-2 rounded w-1/3"
+          />
           <button className="bg-gray-600 text-white p-2 rounded w-1/3">
             Generate
           </button>
