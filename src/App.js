9f3ec552e2cb2421279e77666f01e76e1e2c0356
David Ihl <daih1985@gmail.com>
2023-05-02 15:08:52 +0200

    Add ESLint config

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline bg-red-200">Hello world!</h1>
+    <div className="bg-blue-200 w-screen h-screen">
+      <h1 className="text-3xl font-bold underline bg-red-200">Hello world!</h1>
+    </div>
   );
 }
